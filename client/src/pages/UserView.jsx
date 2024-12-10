ec6531ce7c7e706a62c217b1e185ed23d81d4e50
Igor Eremin <eremin.igor@icloud.com>
2024-12-09 21:26:27 -0800

    rebuilding user view

--- client/src/pages/UserView.jsx
+++ client/src/pages/UserView.jsx
@@ -1,29 +1,54 @@
+import React, { useState } from 'react';
 import Header from '../components/Header';
 import { useParams } from 'react-router-dom';
 
 function UserView() {
   const { id } = useParams();
+  const [activeComponent, setActiveComponent] = useState('Dashboard');
 
-  return (
-    <div className="centered-container">
-      <div className="page-style">
-        <Header title="WORKPULSE" pageName="USER VIEW" />
-        <div className="user-dashboard">
-          <h2 className="dashboard-title">Employee Dashboard</h2>
-          <div className="button-container">
-            <button className="dashboard-button clock-in">Clock In</button>
-            <button className="dashboard-button end-break">End Break</button>
-            <button className="dashboard-button clock-out">Clock Out</button>
-          </div>
-          <h3 className="sessions-title">Today's Sessions</h3>
+  const renderComponent = () => {
+    switch (activeComponent) {
+      case 'Dashboard':
+        return (
           <div className="sessions-card">
+            <h3 className="sessions-title">Today's Sessions</h3>
             <p>Date: 12/5/2024</p>
             <p>Clock In: 10:22:06 PM</p>
             <p>Break Start: 10:22:13 PM</p>
             <p>Break End: 10:22:15 PM</p>
             <p>Clock Out: 10:22:17 PM</p>
           </div>
+        );
+      case 'History':
+        return (
+          <div className="user-component-placeholder">
+            <p>History of attendance will be shown here.</p>
+          </div>
+        );
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div className="centered-container">
+      <div className="page-style">
+        <Header title="WORKPULSE" pageName={`USER VIEW`} />
+        <div className="admin-dashboard">
+          <button
+            className="admin-dashboard-button"
+            onClick={() => setActiveComponent('Dashboard')}
+          >
+            Dashboard
+          </button>
+          <button
+            className="admin-dashboard-button"
+            onClick={() => setActiveComponent('History')}
+          >
+            History
+          </button>
         </div>
+        <div className="admin-content">{renderComponent()}</div>
       </div>
     </div>
   );
